a8725ad0f81ef9db328db45ee3e5d527a5db70c2
whypro <whypro@live.cn>
2015-08-12 22:56:31 +0800

    Modify bae codes

---
+++ bae_run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from bae.core.wsgi import WSGIApplication
+import tornado.wsgi
+
+from searchin import Application
+
+app = Application()
+wsgi_app = tornado.wsgi.WSGIAdapter(app)
+application = WSGIApplication(wsgi_app)
+
