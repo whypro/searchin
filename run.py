deb18e6ea29e7b0c6c93ec13b3012a390102ba74
whypro <whypro@live.cn>
2015-08-13 09:59:46 +0800

    Add heroku config

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
 import tornado.httpserver
 import tornado.ioloop
@@ -9,5 +10,6 @@ from searchin.application import Application
 if __name__ == '__main__':
     app = Application()
     server = tornado.httpserver.HTTPServer(app)
-    server.listen(5000)
+    port = int(os.environ.get("PORT", 5000))
+    server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
