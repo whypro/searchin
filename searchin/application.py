3519da7ef029a5f7e8ecdf5c1b3d6361d86753d1
whypro <whypro@live.cn>
2015-08-25 13:31:58 +0800

    Add favicon

--- searchin/application.py
+++ searchin/application.py
@@ -11,12 +11,6 @@ from .handlers import HomeHandler, PaperSearchHandler, BookSearchHandler
 class Application(tornado.web.Application):
     def __init__(self):
 
-        handlers = [
-            (r'/', HomeHandler),
-            (r'/paper/search', PaperSearchHandler),
-            (r'/book/search', BookSearchHandler),
-        ]
-
         settings = {
             'cookie_secret': "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
             'login_url': '/login',
@@ -24,10 +18,17 @@ class Application(tornado.web.Application):
             'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
             'static_path': os.path.join(os.path.dirname(__file__), 'static'),
             'debug': True,
-            'db_host': '192.168.7.102',
+            'db_host': 'localhost',
             'db_port': 27017,
             'db_name': 'searchin'
         }
+
+        handlers = [
+            (r'/', HomeHandler),
+            (r'/paper/search', PaperSearchHandler),
+            (r'/book/search', BookSearchHandler),
+            (r'/favicon\.ico', tornado.web.StaticFileHandler, dict(path=settings['static_path']))
+        ]
 
         self.client = MongoClient(host=settings['db_host'], port=settings['db_port'])
         self.db = self.client[settings['db_name']]
