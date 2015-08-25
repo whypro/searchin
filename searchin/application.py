e774e5eae46d165be3d91d361c0434459d5aa7a0
whypro <whypro@live.cn>
2015-08-25 13:42:02 +0800

    Modify application.py

--- searchin/application.py
+++ searchin/application.py
@@ -7,29 +7,30 @@ import tornado.web
 
 from .handlers import HomeHandler, PaperSearchHandler, BookSearchHandler
 
+settings = {
+    'cookie_secret': "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+    'login_url': '/login',
+    # 'xsrf_cookies': True,
+    'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+    'static_path': os.path.join(os.path.dirname(__file__), 'static'),
+    'debug': True,
+    'db_host': 'localhost',
+    'db_port': 27017,
+    'db_name': 'searchin'
+}
+
+
+handlers = [
+    (r'/', HomeHandler),
+    (r'/paper/search', PaperSearchHandler),
+    (r'/book/search', BookSearchHandler),
+    (r'/favicon\.ico', tornado.web.StaticFileHandler, dict(path=settings['static_path']))
+]
+
 
 class Application(tornado.web.Application):
     def __init__(self):
-
-        settings = {
-            'cookie_secret': "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-            'login_url': '/login',
-            # 'xsrf_cookies': True,
-            'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
-            'static_path': os.path.join(os.path.dirname(__file__), 'static'),
-            'debug': True,
-            'db_host': 'localhost',
-            'db_port': 27017,
-            'db_name': 'searchin'
-        }
-
-        handlers = [
-            (r'/', HomeHandler),
-            (r'/paper/search', PaperSearchHandler),
-            (r'/book/search', BookSearchHandler),
-            (r'/favicon\.ico', tornado.web.StaticFileHandler, dict(path=settings['static_path']))
-        ]
-
+        
         self.client = MongoClient(host=settings['db_host'], port=settings['db_port'])
         self.db = self.client[settings['db_name']]
 
